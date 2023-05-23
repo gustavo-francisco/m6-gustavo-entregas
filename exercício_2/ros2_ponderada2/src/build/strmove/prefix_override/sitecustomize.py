8ded2130a64dc777164b78e32489646bb8a86c4d
gustavo-francisco <99208114+gustavo-francisco@users.noreply.github.com>
2023-05-23 16:16:14 -0300

    update vídeo
    inserção do vídeo de funcionamento do projeto

--- exercício_2/ros2_ponderada2/src/build/strmove/prefix_override/sitecustomize.py
+++ exercício_2/ros2_ponderada2/src/build/strmove/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/mnt/c/Users/Inteli/Desktop/ros2_ponderada2/src/install/strmove'
+sys.prefix = sys.exec_prefix = '/mnt/c/Users/Inteli/documents/github/m6-gustavo-entregas/exercício_2/ros2_ponderada2/src/install/strmove'
--- exercício_2/ros2_ponderada2/src/install/setup.sh
+++ exercício_2/ros2_ponderada2/src/install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/mnt/c/Users/Inteli/Desktop/ros2_ponderada2/src/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/mnt/c/Users/Inteli/documents/github/m6-gustavo-entregas/exercício_2/ros2_ponderada2/src/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
